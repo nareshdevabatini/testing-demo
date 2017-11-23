e5a0f43bdfdb20a56ed409c8813cbe9817dae733
nareshdevabatini <naresh.devabatini@gmail.com>
2017-11-23 19:48:36 +0530

    integration testing

---
+++ src/app/0000. Integration/2-todos/todos.component.spec.ts
@@ -0,0 +1,37 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+
+describe('TodosComponent', () => {
+  let component: TodosComponent;
+  let fixture: ComponentFixture<TodosComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports:[HttpModule],
+      declarations: [ TodosComponent ],
+      providers:[TodoService], 
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should load todos from the server', () => {
+    let service = TestBed.get(TodoService);
+    spyOn(service,'getTodos').and.returnValue(Observable.from([ [1,2,3] ]));
+
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import{compute}from'../app/small test/01. fundamentals/compute';
+//import{compute}from'../app/small test/01. fundamentals/compute';
 import { AppComponent } from './app.component';
+//import {  } from '';
+//import { Greeter } from './greeter.component.ts/greeter.component.ts.component';
+//import { GreeterComponent } from './greeter/greeter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    //Greeter.Component.TsComponent,
+    //GreeterComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, HttpModule, FormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
